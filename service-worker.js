7beeb0300c5d222e70e355f1b2ebcc6f38470a62
blackbunch <blackbunch@naver.com>
2026-08-27 16:34:52 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = 'blackbunch-schedule-v8';
-const CACHE_NAME = 'blackbunch-schedule-v11';
+const CACHE_NAME = 'blackbunch-schedule-v12';
 const APP_SHELL = [
   './',
   './index.html',
